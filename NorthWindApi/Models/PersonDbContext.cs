375c89e1342c477fac44dcf249c43fb1b9efb3a1
rochaleanne-avanade <rochale.yu@avanade.com>
2023-09-11 09:36:56 +0800

    base files

---
+++ NorthWindApi/Models/PersonDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NorthWindApi.Models
+{
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
